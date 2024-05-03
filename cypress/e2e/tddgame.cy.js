730242344a247060577cab3fd313ca28d724c23a
tanyajohari <johtanya11@gmail.com>
2024-05-03 11:24:02 +0530

    [Tanya] Installed cypress library for e2e testing and defined test suit and test cases

---
+++ cypress/e2e/tddgame.cy.js
@@ -0,0 +1,22 @@
+describe ('Tic Tac Toe App', () => {
+    it ('should display the initial game board', () => {
+        
+    });
+
+    it ('should allow a player to make a move and update the board', () => {
+        
+    });
+
+    it ('should not allow a player to make a move on a filled square', () => {
+        
+    });
+
+    it ('should declare a winner when a row is filled', () => {
+        
+    });
+
+    it('should allow jumping to previous moves using the move history', () => {
+            
+      });
+    
+});
